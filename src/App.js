f353fc6ef8d4585dab09ee821f90fc9aa30fb133
Javier <jav.aravenac@gmail.com>
2023-05-07 21:59:49 -0400

    First Commit Front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const url = 'https://api.github.com';
+
+  useEffect(() => {
+
+
+    //Este metodo se ejecuta siempre por eso siempre carga los usuarios..solo deberia ser con /users
+    const getUsers = async () => {
+
+      const url = 'http://localhost:5000/users';
+      const result = await axios.get(url);
+      console.log("El resultado trae:" + result.data);
+
+      setUsers(result.data);
+    }
+    getUsers();
+
+//Funciona ! Le tiro la ruta en el buscador..y aqui hace la peticion al servidor de NodeJS.
+    const metodoTestFront = async () => {
+
+      const url = 'http://localhost:5000/prueba';
+      const result = await axios.get(url);
+      console.log("metodoTestFront");
+      console.log(result);
+
+    }
+    metodoTestFront();
+      
+    //Prueba
+    // async function fetchData() {
+    //   const response = await axios.get('http://localhost:5000/users');    //siempre trae esta ruta.
+    //   console.log("HOLAAA" );
+    //   setUsers(response.data);
+    // }
+    // fetchData();
+
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Usuarios Github</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.login}>User Id: {user.id} y Node Id: {user.node_id}</li>
+        ))}
+      </ul>
     </div>
   );
 }
